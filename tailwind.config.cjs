7b8da25d2e4b51cc659c6a16e595b9d1f1fd78c8
Kurt Carvey Cadenas <kurtcarveycadenas2001@gmail.com>
2023-08-20 14:23:23 +0800

    added Hero section in Landing page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,19 +4,40 @@ module.exports = {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		extend: {
+			keyframes: {
+				typing: {
+					"0%": {
+						width: "0%",
+						visibility: "hidden",
+					},
+					"100%": {
+						width: "100%",
+					},
+				},
+				blink: {
+					"50%": {
+						borderColor: "transparent",
+					},
+					"100%": {
+						borderColor: "white",
+					},
+				},
+			},
 			backgroundImage: {
 				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+			},
+			animation: {
+				typing: "typing 2s steps(20) infinite alternate, blink .7s infinite",
 			},
 		},
 	},
 	plugins: [
 		plugin(function ({ addUtilities, addComponents }) {
 			addUtilities({
-				".text-rainbow": {
+				".text-gradient-purple": {
 					color: "transparent",
 					backgroundClip: "text",
-					background:
-						"linear-gradient(to right, #498af4, #eb4a3d, #fbbc04, #0f9d58)",
+					background: "linear-gradient(to right, #d8b4fe, #f9a8d4)",
 					WebkitBackgroundClip: "text",
 					WebkitTextFillColor: "transparent",
 				},
