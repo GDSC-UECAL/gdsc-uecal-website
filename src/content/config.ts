0b016725a2c5339990ef5222d2dbc030e0deae95
Deihl_Reyes <78899978+DeihlReyes@users.noreply.github.com>
2023-10-07 00:15:17 +0800

    Revert "Revert "Fix fetching of events and type for image""
    This reverts commit e89cc954a96af55ec340e75907016b498d3c2c4d.

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,7 +2,7 @@ import { defineCollection, z } from "astro:content";
 
 const events = defineCollection({
 	// Type-check frontmatter using a schema
-	schema: z.object({
+	schema: ({ image }) => z.object({
 		title: z.string(),
 		description: z.string(),
 		// Transform string to Date object
@@ -14,7 +14,9 @@ const events = defineCollection({
 			.string()
 			.optional()
 			.transform((str) => (str ? new Date(str) : undefined)),
-		heroImage: z.string().optional(),
+			heroImage: image().refine((img) => img.width >= 1920, {
+				message: "Cover image must be at least 1920 pixels wide!",
+			}),
 	}),
 });
 
