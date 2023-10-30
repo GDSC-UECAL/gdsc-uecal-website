439c3272aff1fab0474d012939789520c6adec6c
Kuruto Denzeru <kurtdenzel51@gmail.com>
2023-10-30 17:24:26 +0800

    Background image

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -25,6 +25,7 @@ module.exports = {
 			},
 			backgroundImage: {
 				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+				
 			},
 			animation: {
 				typing: "typing 2s steps(20) infinite alternate, blink .7s infinite",
