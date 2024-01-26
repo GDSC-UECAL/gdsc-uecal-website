4809f1a1e36f7c339ee9397574583837255d5361
Kuruto Denzeru <kurtdenzel51@gmail.com>
2024-01-26 23:32:39 +0800

    feat: sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://example.com",
+	site: "https://gdsc-uec.vercel.app/",
 	integrations: [mdx(), sitemap(), tailwind()],
 });
