9f6d06732581f5c08072e8a3e2cd910d3df9c18d
Kuruto Denzeru <kurtdenzel51@gmail.com>
2024-01-26 23:38:31 +0800

    refactor: sitemap filter

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,15 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://gdsc-uec.vercel.app/",
-	integrations: [mdx(), sitemap(), tailwind()],
+	integrations: [mdx(
+
+	), sitemap({
+		filter: (page) =>
+			page !== 'https://gdsc-uec.vercel.app/' &&
+			page !== 'https://gdsc-uec.vercel.app/about' &&
+			page !== 'https://gdsc-uec.vercel.app/events'
+	}), tailwind(
+
+	)],
+
 });
