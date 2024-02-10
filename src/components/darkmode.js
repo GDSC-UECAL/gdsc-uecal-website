1967e025c67f08c37a5c733cb2e5d2706813a0be
Kuruto Denzeru <kurtdenzel51@gmail.com>
2024-02-10 23:31:56 +0800

    feat: external dark mode feat

---
+++ src/components/darkmode.js
@@ -0,0 +1,15 @@
+// Icons
+const sunIcon = document.querySelector(".Sun");
+const moonIcon = document.querySelector(".Moon");
+
+// Theme Vars
+
+// Icon Toggling
+
+// Initial Theme Check
+
+// Manual Theme Check
+
+// Call Theme Switch on clicking buttons
+
+// Invoke Theme Check on Initial Load
