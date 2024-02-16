4b1f3ddc95e7406ab27c7cdcec3959bbe799f5d6
Kuruto Denzeru <kurtdenzel51@gmail.com>
2024-02-16 22:41:34 +0800

    feat: added external darkmode script

--- src/components/darkmode.js
+++ src/components/darkmode.js
@@ -3,13 +3,46 @@ const sunIcon = document.querySelector(".Sun");
 const moonIcon = document.querySelector(".Moon");
 
 // Theme Vars
+const userTheme = localStorage.getItem("theme");
+const systemTheme = window.matchMedia("(prefers-colors-scheme: dark)").matches;
 
 // Icon Toggling
+const iconToggle = () => {
+    moonIcon.classList.toggle("display-none");
+    sunIcon.classList.toggle("display-none");
+};
 
 // Initial Theme Check
+const themeCheck = () => {
+    if (userTheme === "dark" || (!userTheme && systemTheme)) {
+        document.documentElement.classList.add("dark");
+        moonIcon.classList.add("display-none");
+        return;
+    }
+    sunIcon.classList.add("display-none");
+};
 
 // Manual Theme Check
+const themeSwitch = () => {
+    if (document.documentElement.classList.contains("dark")) {
+        document.documentElement.classList.remove("dark");
+        localStorage.setItem("theme", "light");
+        iconToggle();
+        return;
+    }
+    document.documentElement.classList.add("dark");
+    localStorage.setItem("theme", "dark");
+    iconToggle();
+};
 
 // Call Theme Switch on clicking buttons
+sunIcon.addEventListener("click", () => {
+    themeSwitch();
+});
+
+moonIcon.addEventListener("click", () => {
+    themeSwitch();
+});
 
 // Invoke Theme Check on Initial Load
+themeCheck();
