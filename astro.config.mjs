b34d4eda937d042a25e238cef51e9ba55a45294e
Kuruto Denzeru <kurtdenzel51@gmail.com>
2024-02-22 22:13:12 +0800

    build: removed astroImageTools

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,11 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import { astroImageTools } from "astro-imagetools";
-
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
 	site: "https://gdsc-uec.vercel.app/",
 
-	integrations: [
-		{ type: 'mdx' },
-		{ type: 'sitemap' },
-		{ type: 'tailwind' },
-		{ type: 'astroImageTools' }
-	],
+	integrations: [mdx(), sitemap(), tailwind()],
 
 });
