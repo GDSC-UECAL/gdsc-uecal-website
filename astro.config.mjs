469352253fc7ff5674b3422bdc592527a5c214c8
Kuruto Denzeru <kurtdenzel51@gmail.com>
2024-02-22 21:45:25 +0800

    build: prefixed Integrations with typings

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,17 +9,11 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
 	site: "https://gdsc-uec.vercel.app/",
 
-	integrations: [astroImageTools, mdx(
-
-	), sitemap({
-		filter: (page) =>
-			page !== 'https://gdsc-uec.vercel.app/' &&
-			page !== 'https://gdsc-uec.vercel.app/about' &&
-			page !== 'https://gdsc-uec.vercel.app/events'
-	}), tailwind(
-
-		),
-
+	integrations: [
+		{ type: 'mdx' },
+		{ type: 'sitemap', options: { filter: (page) => ... } },
+		{ type: 'tailwind' },
+		{ type: 'astroImageTools' }
 	],
 
 });
