6388bf658c7ae79b5efaf16113a1cd619a3ab0cb
Kuruto Denzeru <kurtdenzel51@gmail.com>
2024-04-07 21:37:55 +0800

    build: added partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import partytown from '@astrojs/partytown';
 
 export default defineConfig({
 	site: "https://gdsc-uec.vercel.app/",
 
-	integrations: [mdx(), sitemap(), tailwind()],
+	integrations: [mdx(), sitemap(), partytown(), tailwind()],
 
 });
